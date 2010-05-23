45d23a9a9dd044ff5e706a3c89f746664a58cba6
vikhyat <vikhyatk@gmail.com>
2010-05-23 12:45:06 +0530

    don't display the file extenstion

--- main.rb
+++ main.rb
@@ -38,7 +38,7 @@ end
 
 loop do
   t = $ptr % files.length
-  print "#{files[t]}\n\r"
+  print "#{files[t].delete('.mp3')}\n\r"
   $p.puts "LOAD #{path}#{files[t]}"
   while (a=$p.gets) != "@P 0\n"
     if $break
