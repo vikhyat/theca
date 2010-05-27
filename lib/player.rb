daf39f7c3523fabb298b60aabfe5d294002ac825
vikhyat <vikhyatk@gmail.com>
2010-05-27 18:27:05 +0530

    more player-related code in the Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -3,6 +3,7 @@ class Player
 
   def initialize
     @p = IO.popen("mpg123 -R dummytxt", "w+")
+    @break = false
   end
 
   def load(file)
@@ -21,11 +22,15 @@ class Player
     @p.gets
   end
 
+  def break
+    @break = true
+  end
+
   # start playing and return after we're done.
   def play(file)
     self.load(file)
     while self.gets != STOP_SIGNAL
-      ($break = false; break) if $break
+      (@break = false; break) if @break
     end
   end
 end
